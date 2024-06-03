8b31a297e68d7eb64f49cde43494b6c89aaf51fa
Nantha GS <111976540+NANTHAGS7@users.noreply.github.com>
2024-06-03 19:31:06 +0530

    Time: 3 ms (99.47%) | Memory: 45.2 MB (50.79%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.java
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.java
@@ -1,17 +1,30 @@
 class Solution {
     public int appendCharacters(String s, String t) {
-        int i = 0;
-        int j = 0;
-        while(i<s.length()){
-            while(i<s.length() && j<t.length() && s.charAt(i) != t.charAt(j)){
-                i++;
-            }
-            if(i == s.length() || j == t.length()){
-                return t.length()-j;
-            }
-            i++;
-            j++;
+        // int i = 0;
+        // int j = 0;
+        // while(i<s.length()){
+        //     while(i<s.length() && j<t.length() && s.charAt(i) != t.charAt(j)){
+        //         i++;
+        //     }
+        //     if(i == s.length() || j == t.length()){
+        //         return t.length()-j;
+        //     }
+        //     i++;
+        //     j++;
+        // }
+        // return t.length() - j;
+
+        if(s.equals(t)) return 0;
+        char ss[] = s.toCharArray();
+        char tt[] = t.toCharArray();
+        int j=0;
+        int m = ss.length;
+        int n = tt.length;
+        for(int i=0;i<m;i++){
+            if(ss[i] == tt[j]) j++;
+
+            if(j == n) return 0;
         }
-        return t.length() - j;
+        return n - j;
     }
 }
