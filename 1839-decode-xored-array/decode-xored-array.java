b86c824c8b4502e7a042231b800097da9654f325
Nantha GS <111976540+NANTHAGS7@users.noreply.github.com>
2024-06-04 01:34:59 +0530

    Time: 2 ms (89.19%) | Memory: 45.6 MB (38.54%) - LeetSync

---
+++ 1839-decode-xored-array/decode-xored-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        int n = encoded.length;
+        int arr[] = new int[n+1];
+        arr[0] = first;
+        for(int i=1;i<n+1;i++){
+            arr[i] = first ^ encoded[i-1];
+            first = arr[i];
+        }
+        return arr;
+    }
+}
