0bc161898d82fcb95b873daf4277a29000865264
Nantha GS <nanthadgl2003@gmail.com>
2024-07-09 00:56:42 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (54.76%) - LeetSync

--- 1642-water-bottles/water-bottles.java
+++ 1642-water-bottles/water-bottles.java
@@ -1,10 +1,12 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-        int res = numBottles;
-        while(numBottles >= numExchange){
-            res += numBottles/numExchange;
-            numBottles = numBottles/numExchange + numBottles % numExchange;
-        }
-        return res;
+        // int res = numBottles;
+        // while(numBottles >= numExchange){
+        //     res += numBottles/numExchange;
+        //     numBottles = numBottles/numExchange + numBottles % numExchange;
+        // }
+        // return res;
+
+        return numBottles + (numBottles - 1)/(numExchange - 1);     //O(1) -> constant space
     }
 }
