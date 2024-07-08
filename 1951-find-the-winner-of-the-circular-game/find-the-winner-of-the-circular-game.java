e7962b013aa86e7bfb03ecd32f0494237268527b
Nantha GS <nanthadgl2003@gmail.com>
2024-07-08 22:32:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (65.78%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,25 +1,30 @@
 class Solution {
 
-    public static int solve(int n,int k){
-        if(n == 1) return 0;
-        return (solve(n-1,k)+k)%n;
-    }
+    // public static int solve(int n,int k){
+    //     if(n == 1) return 0;
+    //     return (solve(n-1,k)+k)%n;                  //recursive o(n)
+    // }
 
     public int findTheWinner(int n, int k) {
-        // Queue<Integer> q = new LinkedList<>();
+        // // Queue<Integer> q = new LinkedList<>();
 
-        // for(int i=1;i<=n;i++){
-        //     q.add(i);
-        // }
+        // // for(int i=1;i<=n;i++){
+        // //     q.add(i);
+        // // }
 
-        // while(q.size() > 1){
-        //     for(int j=1;j<k;j++){
-        //         q.add(q.peek());
-        //         q.poll();
-        //     }
-        //     q.poll();
-        // }
-        // return q.peek();
-        return solve(n,k)+1;
+        // // while(q.size() > 1){
+        // //     for(int j=1;j<k;j++){
+        // //         q.add(q.peek());
+        // //         q.poll();
+        // //     }
+        // //     q.poll();
+        // // }
+        // // return q.peek();
+        // return solve(n,k)+1;
+        int win = 0;                                 
+        for(int i=2;i<=n;i++){
+            win = (win + k) % i;                        ///constant space   o(1)
+        }
+        return win + 1;
     }
 }
