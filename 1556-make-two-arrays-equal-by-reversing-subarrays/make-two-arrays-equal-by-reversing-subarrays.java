1ec8078a03bb48cac0ba84f0f035d03216925c43
Nantha GS <nanthadgl2003@gmail.com>
2024-08-03 11:45:25 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (73.75%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,9 +1,18 @@
 class Solution {
   public boolean canBeEqual(int[] target, int[] arr) {
-    return Arrays.stream(arr)
-        .boxed()
-        .collect(Collectors.groupingBy(Integer::intValue, Collectors.counting()))
-        .equals(Arrays.stream(target).boxed().collect(
-            Collectors.groupingBy(Integer::intValue, Collectors.counting())));
+    int n = target.length;
+    int m = arr.length;
+    if (n != m) return false;
+    int[] soln = new int[1001];
+    for (int i = 0; i < n; i++) {
+        soln[target[i]]++;
+        soln[arr[i]]--;
+    }
+    for (int i = 0; i < n; i++) {
+        if (soln[target[i]] != 0 || soln[arr[i]] != 0) {
+            return false;
+        }
+    }
+    return true;
   }
 }
